3cf2ac99851d71d1a2afaad4883c2c896b1eb31b
SimplyGreenIT <ophilippot@greenspector.com>
2017-06-16 15:17:08 +0200

    Projet Example avec API XAMARIN intégrée

---
+++ CreditCardValidator.Droid.UITests/Tests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+
+
+namespace CreditCardValidator.Droid.UITests
+{
+	[TestFixture]
+	public class Tests
+	{
+		AndroidApp app;
+
+        [SetUp]
+		public void BeforeEachTest()
+		{
+			app = ConfigureApp.Android.InstalledApp("com.xamarin.example.creditcardvalidator").StartApp();
+            //Greenspector Measure Set Up
+            app.Invoke("SetUpMeasureBackdoor", "kOrscsBn3urw9096fS1z6FMrLV66ijgH");
+        }
+
+        [Test]
+        public void CreditCardNumber_TooShort_DisplayErrorMessage()
+        {
+            //Start of Greenspector measure
+            app.Invoke("StartMeasureBackdoor");
+
+            app.WaitForElement(c => c.Marked("action_bar_title").Text("Enter Credit Card Number"));
+            app.EnterText(c => c.Marked("creditCardNumberText"), new string('9', 15));
+            app.Tap(c => c.Marked("validateButton"));
+
+            app.WaitForElement(c => c.Marked("errorMessagesText").Text("Credit card number is too short."));
+
+            //Stop of Greenspector measure
+            app.Invoke("StopMeasureBackdoor","TestBackdoor");
+        }
+
+    }
+}
+
---
+++ CreditCardValidator.Droid/MainActivity.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using CreditCardValidation.Common;
+using System.Threading;
+
+/*Needed references (need to be integrate in References*/
+using Com.Greenspector.Probe.Android.Interfaces;
+using Java.Interop;
+
+namespace CreditCardValidator.Droid
+{
+
+	[Activity(Label = "@string/activity_main", MainLauncher = true, Theme = "@android:style/Theme.Holo.Light")]
+	public class MainActivity : Activity
+	{
+
+        /*Greenspector Backdoor method to call on Xamarin.UITests*/
+        Api gspt;
+
+        [Export("SetUpMeasureBackdoor")]
+        public void SetUpMeasureBackdoor(String token)
+        {
+            gspt = new Api();
+            // CHANGE PARAMETERS
+            ThreadPool.QueueUserWorkItem(o => gspt.SetUpMeasure(ApplicationContext, new String[] { "com.xamarin.example.creditcardvalidator" }, null, "XamarinCreditCard", "1", "https://app.greenspector.com/api", token, 180, 200, true));
+        }
+
+        [Export("StartMeasureBackdoor")]
+        public void StartMeasureBackdoor()
+        {
+            gspt.StartMeasure();
+        }
+
+        [Export("StopMeasureBackdoor")]
+        public void StopMeasureBackdoor(String TestName)
+        {
+            ThreadPool.QueueUserWorkItem(o => gspt.StopMeasure(TestName));
+        }
+        /*End of Greenspector Backdoor*/
+
+
+        static readonly ICreditCardValidator _validator = new SimpleCreditCardValidator();
+
+		EditText _creditCardTextField;
+		TextView _errorMessagesTextField;
+		Button _validateButton;
+
+        protected override void OnCreate(Bundle bundle)
+		{
+                      
+
+            base.OnCreate(bundle);
+
+			SetContentView(Resource.Layout.Main);
+					
+			_errorMessagesTextField = FindViewById<TextView>(Resource.Id.errorMessagesText);
+			_creditCardTextField = FindViewById<EditText>(Resource.Id.creditCardNumberText);
+			_validateButton = FindViewById<Button>(Resource.Id.validateButton);
+			_validateButton.Click += (sender, e) =>{
+				_errorMessagesTextField.Text = String.Empty;
+				string errMessage;
+
+				if (_validator.IsCCValid(_creditCardTextField.Text, out errMessage))
+				{
+					var i = new Intent(this, typeof(CreditCardValidationSuccess));
+					StartActivity(i);
+                }
+				else
+				{
+					RunOnUiThread(() => { _errorMessagesTextField.Text = errMessage; });
+                }
+			};
+		}
+	}
+}
+
+
